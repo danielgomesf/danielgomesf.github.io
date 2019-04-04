aecbeb0798943dcb3c14811973b33848b5c09230
Dfg98 <danielgomesfalcao@gmail.com>
2019-04-04 04:15:17 -0300

    Criado botoes de curriculo e contato

--- JAVASCRIPT/Javascript.js
+++ JAVASCRIPT/Javascript.js
@@ -23,3 +23,29 @@ var x = document.getElementById ("meuNav")
    else {x.className = "menu"}
 } 
 }
+
+{var box = document.getElementById('meuModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("meuBt");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("fechar")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  box.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  box.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == box) {
+    box.style.display = "none";
+  }
+}
+}
